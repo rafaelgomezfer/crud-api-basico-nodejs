3df02f53fa6e1b1a6f1d50560446b72ea74df4b0
Rafael Gomez <rafagf89@gmail.com>
2020-07-30 01:26:15 -0500

    actualizar y eliminar usuario

--- server.js
+++ server.js
@@ -21,11 +21,36 @@ app.get('/api/users', (req, res)=>{
 
 app.post('/api/users', (req, res)=>{
     usersDB.push(req.body);
+    
     res.status(201).send({
         success: 'true',
         message:'Data added successfully'
     })
 });
+
+app.put('/api/users/:id', (req, res)=>{
+    const ID = req.params.id;
+    let userIndex= usersDB.findIndex(user=>user.id===ID);
+    let data = req.body;
+    usersDB[userIndex].id=data.id;
+    usersDB[userIndex].name=data.name;
+    
+    res.status(201).send({
+        success: 'true',
+        message:'user updated successfully'
+    })
+});
+
+app.delete('/api/users/:id', (req, res)=>{
+    const ID = req.params.id;
+    let userIndex= usersDB.findIndex(user=>user.id===ID);
+    usersDB.splice(userIndex, 1);
+    
+    res.status(201).send({
+        success: 'true',
+        message:'user deleted successfully'
+    })
+})
 
 app.listen(PORT, ()=>{
     console.log(`server runnning on port ${PORT}`);
